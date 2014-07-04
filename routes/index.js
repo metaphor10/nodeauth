5f8391dcb3ccf0aebc531dfdc5822af42804e63a
Ariel <borochov@american.edu>
2014-07-04 15:00:01 -0400

    have the login screen
    added the login screen and signup. passport working and so is express.
    Very close to haveing it all work

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(passport){
+// normal routes ===============================================================
+
+	// show the home page (will also have our login links)
+	router.get('/', function(req, res) {
+		res.render('index', {title: 'Node Authentication'});
+	});
+
+	// PROFILE SECTION =========================
+	router.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile', {
+			user : req.user
+		});
+	});
+
+	// LOGOUT ==============================
+	router.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+	// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+	// locally --------------------------------
+		// LOGIN ===============================
+		// show the login form
+		router.get('/login', function(req, res) {
+			res.render('login', { message: req.flash('loginMessage') });
+		});
+
+		// process the login form
+		router.post('/login', passport.authenticate('local-login', {
+			successRedirect : '/profile', // redirect to the secure profile section
+			failureRedirect : '/login', // redirect back to the signup page if there is an error
+			failureFlash : true // allow flash messages
+		}));
+
+		// SIGNUP =================================
+		// show the signup form
+		router.get('/signup', function(req, res) {
+			res.render('signup', { message: req.flash('signupMessage') });
+		});
+
+		// process the signup form
+		router.post('/signup', passport.authenticate('local-signup', {
+			successRedirect : '/profile', // redirect to the secure profile section
+			failureRedirect : '/signup', // redirect back to the signup page if there is an error
+			failureFlash : true // allow flash messages
+		}));
+
+
+ return router;
+};
+
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+		return next();
+
+	res.redirect('/');
+}
